9637fbba4c5bc95357ca5f63ecccd50db0aee384
AhPro7 <ahpro001@gmail.com>
2023-01-05 17:35:40 +0200

    save

--- beta_with_gui/main_test.py
+++ beta_with_gui/main_test.py
@@ -1,5 +1,11 @@
 # test the moedule
 from pdf_pre import *
-path = '../alpha/test.pdf'
-pdf = pdf_pre(path)
-pdf.save_pra('beta_with_gui/test.txt')
+# path = 'book/test.pdf'
+# pdf = pdf_pre(path)
+# pdf.save_pra('beta_with_gui/test.txt')
+# test the module top_3
+path = 'beta_with_gui/test.txt'
+top = top_3(path)
+top.get_top_3('what is an image classification?')
+top_33=top.save_top_3('what is an image classification?','beta_with_gui/top.txt')
+print(top_33[0])
--- beta_with_gui/pdf_pre.py
+++ beta_with_gui/pdf_pre.py
@@ -37,6 +37,8 @@ def save_pra(pra, path):
         for i in pra:
             f.write(i +'\n\n\n'+ '------------------------' +'\n\n\n')
 
+
+# ____________________________________________________________________________
 # make the module 
 class pdf_pre:
     def __init__(self, path):
@@ -47,3 +49,59 @@ class pdf_pre:
     
     def save_pra(self, path):
         save_pra(self.pra, path)
+# ____________________________________________________________________________
+
+import nltk
+nltk.download('punkt')
+
+# read the paragraph and the question
+#check if the question answer may be in the paragraph
+#return the score
+def score(pra, question):
+    # get the question words
+    question_words = nltk.word_tokenize(question)
+    # get the paragraph words
+    pra_words = nltk.word_tokenize(pra)
+    # get the intersection of the question words and the paragraph words
+    intersection = set(question_words).intersection(set(pra_words))
+    # get the score
+    score = len(intersection)/len(question_words)
+    return score
+
+# read the text file with all paragraphs with "'\n\n\n'+ '------------------------' +'\n\n\n'" as the separator
+# return a list of paragraphs
+def read_text_file(file_name):
+    with open(file_name, 'r') as f:
+        text = f.read()
+    return text.split('\n\n\n'+ '------------------------' +'\n\n\n')
+
+# get top 3 paragraphs
+def get_top_3(pra_list, question):
+    # get the score of each paragraph
+    score_list = []
+    for pra in pra_list:
+        score_list.append(score(pra, question))
+    # get the top 3 paragraphs
+    top_3 = []
+    for i in range(3):
+        top_3.append(pra_list[score_list.index(max(score_list))])
+        score_list[score_list.index(max(score_list))] = 0
+    return top_3
+
+
+class top_3:
+    def __init__(self, path):
+        self.path = path
+        self.pra_list = read_text_file(self.path)
+
+    def get_top_3(self, question):
+        return get_top_3(self.pra_list, question)
+    #  save the top 3 paragraphs in a text file
+    def save_top_3(self, question, path):
+        top_3 = self.get_top_3(question)
+        with open(path, 'w') as f:
+            for pra in top_3:
+                f.write(pra +'\n\n\n'+ '------------------------' +'\n\n\n')
+        return top_3
+
+
